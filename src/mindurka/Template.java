a4cf1115781fafc8ebfeb2046c4b779d24e850d4
janu8ry <68150356+janu8ry@users.noreply.github.com>
2021-08-12 20:13:44 +0900

    Initial commit

---
+++ src/mindurka/Template.java
@@ -0,0 +1,23 @@
+package mindurka;
+
+import arc.util.*;
+
+import mindustry.mod.*;
+
+public class Template extends Plugin {
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void registerClientCommands(CommandHandler handler) {
+
+    }
+
+    @Override
+    public void registerServerCommands(CommandHandler handler) {
+
+    }
+}
